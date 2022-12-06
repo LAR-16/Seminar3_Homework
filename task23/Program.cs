da83bdccd1d0faca6c83412a8d57d4377bc50eb4
Alexandra <lebedeva1618@yandex.ru>
2022-12-06 19:14:58 +0300

    возведение чисел в куб

---
+++ task23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int x = 1;
+while (x <= n)
+{
+    Console.WriteLine(Math.Pow(x, 3));
+    x ++;
+}
